6f4bb3868a15ad20d2ab341f15ad7e9a7f3aad61
alialgallaf <alialgallaf06@gmail.com>
2025-08-23 08:31:47 +0300

    finish

--- app.js
+++ app.js
@@ -19,10 +19,23 @@ let correctchars = 0;
 let totalChars = 0;
 let timers;
 let timeleft = 30;
+document.getElementById("input-text").disabled = true;
+document.getElementById("input-text").style.visibility = "hidden";
+document.getElementById("timer").style.visibility = "hidden";
+document.getElementById("game-title").style.fontSize = "80px";
+document.getElementById("game-level").style.fontSize = "35px";
+
+const buttons = document.querySelectorAll("#difficulty-buttons button");
+buttons.forEach( btn => {
+btn.style.fontSize = "20px";
+btn.style.width = "200px";
+btn.style.height = "150px";
+}
+);
+
 
 // UI ELEMENTS
 const timerEl = document.getElementById("timer");
-const startButton = document.getElementById("start-button");
 const easyButton = document.getElementById("easy-button");
 const mediumButton = document.getElementById("medium-button");
 const hardButton = document.getElementById("hard-button");
@@ -38,19 +51,28 @@ function startGame(difficulty) {
             targetSentence = sentences_hard[Math.floor(Math.random() * sentences_hard.length)];
     }
 
-    setTheme();
-
     timeleft = 30;
     timerEl.innerHTML = `Time is ${timeleft}`;
     document.getElementById("input-text").disabled = false;
     document.getElementById("target-text").innerHTML = targetSentence;
     document.getElementById("input-text").value = "";
-    document.getElementById("results").innerHTML = "";
-    document.getElementById("best-score").innerHTML = "";
     document.getElementById("input-text").addEventListener("input" , texttyped)
+    document.getElementById("input-text").style.visibility = "visible";
+    document.getElementById("timer").style.visibility = "visible";
+    document.getElementById("game-level").style.display = "none";
+    document.getElementById("instaqshin").style.display = "none";
+
     clearInterval(timers);
     starttimer();
+    buttons.forEach( btn => {
+        btn.style.fontSize = "20px";
+        btn.style.width = "100px";
+        btn.style.height ="100px";
 }
+);
+document.getElementById("game-title").style.fontSize = "50px";
+}
+
 
 function setTheme(level) {
     document.body.className = level;
@@ -65,6 +87,13 @@ function starttimer() {
         clearInterval(timers);
         document.getElementById("input-text").removeEventListener("input" , texttyped);
     }
+    if (timeleft < 10) {
+    timerEl.style.borderColor = "#e74c3c";
+    timerEl.style.color = "#e74c3c";
+    } else {
+    timerEl.style.borderColor = "#3498db"; 
+    timerEl.style.color = "#2c3e50";
+}
     }
     , 1000)
 }
@@ -72,17 +101,25 @@ function starttimer() {
 
 function texttyped () {
     let inputtext = document.getElementById("input-text").value;
-
     correctchars = 0;
+    let displayHTML = "";
 
     for ( let i = 0 ; i < targetSentence.length; i++) {
-        if (inputtext[i] === targetSentence[i]) correctchars++;  
-    }
-
-    if ( inputtext === targetSentence) {
-        gameover (true);
-    }
-}
+        if (i < inputtext.length) {
+            if (inputtext[i] === targetSentence[i]) {
+            displayHTML += `<span class = 'correct'>${targetSentence[i]}</span>`;
+            correctchars++;  
+            } else {
+             displayHTML += `<span class = 'incorrect'>${targetSentence[i]}</span>`;
+             }
+        } else {
+        displayHTML += targetSentence[i];
+        }}
+    if (inputtext.length >= targetSentence.length) {
+         gameover(inputtext === targetSentence); 
+        }
+    document.getElementById("target-text").innerHTML = displayHTML;
+};
 
 
 function gameover (isOver) {
@@ -90,21 +127,33 @@ function gameover (isOver) {
         document.getElementById("input-text").disabled =true;
         clearInterval(timers);
 
+        let popup = document.getElementById("popup");
+        let popuptitle = document.getElementById("popup-title");
+        let popupmassege = document.getElementById("popup-messege");
+        let popupcontent = document.getElementById("popup-content");
+        popup.style.display = "flex";
+        popupcontent.style.display = "flex";
+
+
     if (isOver) {
-        let typedwords = document.getElementById("input-text").value.split(" ").length;
         let timetaken =30 - timeleft;
-        let correctchars2 = typedwords / 5;
-        let wpm = (correctchars2 / timetaken) ;
+        let typedwords = document.getElementById("input-text").value.split(" ").length;
+        let wpm = Math.floor(typedwords * 60 / timetaken) ;
+
         if (wpm > best_score) {
             best_score = wpm;
         }
-        document.getElementById("results").innerHTML = `You won! time taken: ${timetaken} sec ,the correct characters is ${correctchars}`;
-        document.getElementById("best-score").innerHTML = `Best score: ${best_score} WPM`;
-
+        popuptitle.innerHTML = "You Won!";
+        popupmassege.innerHTML = ` time taken: ${timetaken} sec ,the correct characters is ${correctchars}, WPM : ${wpm},
+        Best score: ${best_score} WPM`; 
     } else {
-        document.getElementById("results").innerHTML = `You lose!`;
+        let timetaken =30 - timeleft;
+        let typedwords = document.getElementById("input-text").value.split(" ").length;
+        let wpm = Math.floor(typedwords * 60 / timetaken) ;
+        popuptitle.innerHTML = "You lose!";
+        popupmassege.innerHTML = ` time taken: ${timetaken} sec , WPM : ${wpm},
+        Best score: ${best_score} WPM`; 
     }
-
 };
 
 
@@ -124,3 +173,7 @@ hardButton.addEventListener("click", (evt) => {
     startGame('hard'); 
     setTheme('hard')  
 });
+
+document.getElementById("popup-close").addEventListener("click" , () => {
+              document.getElementById("popup").style.display = "none";
+        });
